234c10c7a623d174a6d00c67234b00458f1b27bd
Wolfgang Ziegler // fago <fago@wolfgangziegler.net>
2021-04-13 16:51:20 +0200

    Add menu support.

---
+++ demo-frontend/components/global/Node.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="node">
+    <h2>{{ title }}</h2>
+    <slot name="body" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+  }
+}
+</script>
